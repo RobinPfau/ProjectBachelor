67e120fd8f0f27570c3eb7467f5a9f02bd314dd3
Robin Pfau <robinpfau94@gmx.de>
2024-10-31 11:54:40 +0100

    non functioning basic gui

--- straights/GUI/gui2.py
+++ straights/GUI/gui2.py
@@ -16,7 +16,8 @@ class App (ctk.CTk):
 
         #widgets
         self.Puzzle = Puzzle(self, 0, 0, "green", 0.65, 0.65)
-        self.Controls = Controls(self, 0.7, 0, "red", 0.3, 1)
+        self.Controls = Controls(self, 0.7, 0, "black", 0.3, 0.65)
+        self.Controls = Solver(self, 0, 0.7, "green", 0.65, 0.3)
 
         #run
         self.mainloop()
@@ -39,25 +40,55 @@ class Puzzle(ctk.CTkFrame):
                 frame.grid_columnconfigure(col, weight=1)
 
                 cell = ctk.CTkEntry(frame, width = 40, height = 40, font=("Arial", 24), justify = "center")
+                #placeholder filling of grid
+                #cell.insert(0, "1")
                 cell.grid(row = row, column = col, fill = None)
 
        
         frame.pack(expand = False, fill= None, anchor = "nw", padx = 10, pady = 10)
-
-       
 
 class Controls(ctk.CTkFrame):
     def __init__(self, parent, x, y, colour, rwidth, rheight, **kwargs):
 
         #setup control menu
         super().__init__(parent, **kwargs)
-        self.place(relx = x, rely= y, relwidth = rwidth, relheight = rheight,)
+        self.place(relx = x, rely= y, relwidth = rwidth, relheight = rheight)
     
+        #making the numpad
+        frame = ctk.CTkFrame(self, )
+        buttonnumber = 1
+        for row in range(3):
+             for col in range(3):
+                
+                frame.grid_rowconfigure(row, weight=1)
+                frame.grid_columnconfigure(col, weight=1)
 
+                button = ctk.CTkButton(frame, fg_color = colour)
+                button.configure(text = buttonnumber)
+                buttonnumber = buttonnumber + 1
+                button.grid(row = row, column = col, fill = None)
 
+       
+        frame.pack(expand = False, fill= None, anchor = "center", padx = 10, pady = 10)
 
-        #placeholder for visibility of frame
-        #ctk.CTkLabel(self, bg_color=colour).pack(expand = True, fill = "both",)
+class Solver(ctk.CTkFrame):
+    def __init__(self, parent, x, y, colour, rwidth, rheight, **kwargs):
 
+        #setup solver menu
+        super().__init__(parent, **kwargs)
+        self.place(relx = x, rely= y, relwidth = rwidth, relheight = rheight)
+    
+        #making the solver controls
+        frame = ctk.CTkFrame(self, )
+        frame.grid_rowconfigure(1, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
+
+        button_solve = ctk.CTkButton(frame, fg_color = colour, text="solve")
+        button_load = ctk.CTkButton(frame, fg_color = "dark green", text="load")
+        button_solve.grid(row = 0, column = 0, fill = None)
+        button_load.grid(row = 1, column = 0, fill = None)
+
+       
+        frame.pack(expand = False, fill= None, anchor = "center", padx = 10, pady = 10)
 
 App("Str8ts Solver", (500,500))
