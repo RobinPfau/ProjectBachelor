20064e4e03991d29c68128de9c571312f13acbc8
Robin Pfau <robinpfau94@gmx.de>
2024-11-06 22:21:35 +0100

    added puzzleselect added placeholer buttons changed look

--- straights/GUI/controls.py
+++ straights/GUI/controls.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
+from converter import Converter
+
 
 class Controls(ctk.CTkFrame):
-    def __init__(self, parent, x, y, colour, rwidth, rheight, grid, matrix, **kwargs):
+    def __init__(self, parent, x, y, colour, rwidth, rheight, grid, matrix, json_keys,  **kwargs):
 
         #setup control menu
         super().__init__(parent, **kwargs)
@@ -9,9 +11,14 @@ class Controls(ctk.CTkFrame):
         self.button_number = 1
         self.grid = grid
         self.matrix = matrix
+        self.keys = json_keys
+        self.converter = Converter()
+
         self.create_numpad(colour)
         self.create_control_buttons()
-
+        self.create_loading()
+                
+    
     #creates the numpad
     def create_numpad(self, colour):
         
@@ -34,13 +41,47 @@ class Controls(ctk.CTkFrame):
 
     #creates the delete button and maybe more
     def create_control_buttons(self):
-        button_delete = ctk.CTkButton(self, text = "DELETE" ,command=  lambda: self.on_press_delete())
-        button_delete.pack(padx = 10, pady= 10)
 
-        button_load = ctk.CTkButton(self, text = "LOAD" ,command=  lambda: self.on_press_load())
-        button_load.pack(padx = 10, pady= 10)
-         
-    #functionality of the delete button
+        frame = ctk.CTkFrame(self, )
+
+        for row in range(3):
+             for col in range(2):
+                frame.grid_rowconfigure(row, weight=1)
+                frame.grid_columnconfigure(col, weight=1)
+
+
+        button_delete = ctk.CTkButton(frame, width = 100, text = "DELETE" ,command=  lambda: self.on_press_delete())
+        button_delete.grid(row = 0, column = 0, fill = None, pady = 10, padx =5)
+
+        button_notes = ctk.CTkButton(frame, width = 100, text = "NOTES" ,command=  lambda: self.on_press_toggle_notes())
+        button_notes.grid(row = 0, column = 1, fill = None, pady = 10, padx =5)
+
+
+
+        button_solve = ctk.CTkButton(frame, width = 100, text = "SOLVE" ,command=  lambda: self.on_press_solve())
+        button_solve.grid(row = 2, column = 0, fill = None, pady = 10, padx =5)
+
+        button_help = ctk.CTkButton(frame, width = 100, text = "HELP" ,command=  lambda: self.on_press_help())
+        button_help.grid(row = 2, column = 1, fill = None, pady = 10, padx =5)
+
+        frame.pack(expand = False, fill = None, anchor = "center", padx = 10, pady = 10)
+        
+        #button_test = ctk.CTkButton(self, text = "TEST" ,command=  lambda: self.on_press_test())
+        #button_test.pack(padx = 10, pady= 10)
+
+
+    def create_loading(self):
+        self.options_main = ctk.CTkOptionMenu(self, values = list(self.keys.keys()), command = self.on_press_select)
+        self.options_main.set("Choose Category")
+        self.options_main.pack()
+
+        
+        self.options_sub = ctk.CTkOptionMenu(self, values = ["Choose Category First"], command = self.on_press_subselect)
+        self.options_sub.set("Coose Difficulty")
+        self.options_sub.pack()
+
+
+    #button that clears the selected cell
     def on_press_delete(self):
         focused_cell = self.grid.selected_cell
         if focused_cell is not None:
@@ -52,7 +93,7 @@ class Controls(ctk.CTkFrame):
         else: 
             print("no active cell")
 
-    #functionality of the numpad buttons
+    #numpad buttons that enters the pressed number into cell
     def on_press_number(self, number):
         #print(number)
         focused_cell = self.grid.selected_cell
@@ -64,8 +105,40 @@ class Controls(ctk.CTkFrame):
         else:
             print("no active cell")
 
-    def on_press_load(self):
-        #todo: load a puzzle from json through converter
+    #dropdown  for puzzletype selection
+    def on_press_select(self, main_cat):
+        
+        sub_cats = list(self.keys[main_cat].keys())
+        self.options_sub.configure(values=sub_cats)
+        self.options_sub.set("Select a Difficulty")
+        
+     #dropdown  for difficulty selection
+    def on_press_subselect(self, sub_cat):
+        
+        selected_main = self.options_main.get()
+        #self.options_main.configure(state = "disabled")
+        #self.options_sub.configure(state = "disabled")
+
+        # TODO: only selecting first puzzle in category. maybe random?
+        selected_puzzle = self.converter.convert(self.keys[selected_main][sub_cat][next(iter(self.keys[selected_main][sub_cat]))])
+        
+        self.matrix.reload_matrix(selected_puzzle)
+        self.grid.delete_grid()
         self.grid.create_grid()
-        print("newGrid")
+
+        print(f"Selected path: {selected_main}/{sub_cat}")
+
+    # TODO: start the solver and load solution into grid
+    def on_press_solve(self):
+        self.matrix.update_matrix(1,1,7)
+
+    # TODO: offer a not yet defined help
+    def on_press_help(self):
+        pass
+
+    # TODO: toggle to not taking in grid
+    def on_press_toggle_notes(self):
+        pass
+
+        
       
